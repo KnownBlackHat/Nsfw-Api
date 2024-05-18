84da55b79e1999c5b9701216d6a1d60d71c441ce
Known_Black_Hat <knownblackhat@gmail.com>
2024-05-18 06:25:20 -0400

    refactor: try to follow vercel py project schema

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import uvicorn
 
+from api.index import app
+
 if __name__ == "__main__":
-    uvicorn.run("app.__main__:app", host="0.0.0.0", port=9999, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=9999, reload=True)
